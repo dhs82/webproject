3069b2273e19e7311e9d5ffa7f9f9d16be50ae2e
dhs82 <dh.squat@gmail.com>
2024-05-15 13:37:44 +0900

    first commit

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('', views.homelands, name='homelands'),
+    path('info/', views.info, name='info'),
+    path('bookmark/', views.bookmark, name='bookmark'),
+    path('recommend/', views.recommend, name='recommend'),
+]
---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bookmark(request):
+    return render(request,'main/bookmark.html')
+def info(request):
+    return render(request,'main/info.html')
+def recommend(request):
+    return render(request,'main/recommend.html')
+def homelands(request):
+    return render(request,'main/homelands.html')
