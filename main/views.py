195328f22cee8513be9e232c62ed090e459a1304
WonsickChoi <king258436@naver.com>
2024-06-03 15:23:16 +0900

    info(상세페이지) 바꾸고 파이썬 코드들은 다른 파일들과 똑같이 test 코드 추가한다고 바꿨습니다. 또 현재 상영중인 영화 이미지 몇개 크롤링 해왔어요

--- main/views.py
+++ main/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def bookmark(request):
-    return render(request,'main/bookmark.html')
+    return render(request, 'main/bookmark.html')
+
 def info(request):
-    return render(request,'main/info.html')
+    return render(request, 'main/info.html')
+
 def recommend(request):
-    return render(request,'main/recommend.html')
+    return render(request, 'main/recommend.html')
+
 def homelands(request):
-    return render(request,'main/homelands.html')
+    return render(request, 'main/homelands.html')
+
+def test(request):
+    return render(request, 'main/test.html')
